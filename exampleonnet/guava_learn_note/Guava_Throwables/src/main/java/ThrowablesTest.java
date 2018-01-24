df8f18ac635d1e47c066cb8a9056a1c530fdf17c
holdbelief <81971357@qq.com>
2018-01-24 22:32:54 +0800

    1

--- exampleonnet/guava_learn_note/Guava_Throwables/src/main/java/ThrowablesTest.java
+++ exampleonnet/guava_learn_note/Guava_Throwables/src/main/java/ThrowablesTest.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 import org.junit.Test;
 
@@ -17,16 +19,38 @@ public class ThrowablesTest {
 		}
 	}
 
-	@Test
-	public void call() throws IOException  {
+	public void throwIfInstanceOf() throws IOException {
 		try {
-			throw new IOException();
+			throw new IOException("测试IOException");
 		} catch (Throwable t) {
-//			Throwables.propagateIfInstanceOf(t, IOException.class);
-//			throw Throwables.propagate(t);
-			
+			// Throwables.propagateIfInstanceOf(t, IOException.class);
+			// throw Throwables.propagate(t);
+
 			Throwables.throwIfInstanceOf(t, IOException.class);
 			throw new AssertionError(t);
+		}
+	}
+
+	public void testCheckException() {
+		try {
+			URL url = new URL("http://ociweb.com"); // 会抛出IOException
+			final InputStream in = url.openStream();
+			// read from the input stream
+			in.close();
+		} catch (Throwable t) {
+			// Throwables.throwIfInstanceOf(t, IOException.class);
+			Throwables.throwIfUnchecked(t);
+			throw new AssertionError(t);
+		}
+	}
+
+	@Test
+	public void test() {
+		try {
+			throwIfInstanceOf();
+			System.out.println("没捕获到异常");
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
